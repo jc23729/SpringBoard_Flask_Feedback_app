c4c67d438e7497d6f5f35670fc9fcf56367ad68d
Juan Cruz <jc23729@gmail.com>
2021-01-10 15:44:05 -0500

    requirements.txt file created

--- venv/app.py
+++ venv/app.py
@@ -1,3 +1,9 @@
-from flask import flask
+from flask import flask, redirect, session
 
 app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    """Homepage of site; redirect to register."""
+
+    return redirect("/register")
