90b2c81d742ee4912a9f9aa1ad90bd80a9a98e9e
Juan Cruz <jc23729@gmail.com>
2021-01-10 16:01:04 -0500

    update

--- venv/app.py
+++ venv/app.py
@@ -1,9 +1,29 @@
-from flask import flask, redirect, session
+"""Feedback Flask app."""
+
+from flask import Flask, render_template, redirect, session
+from flask_debugtoolbar import DebugToolbarExtension
+from werkzeug.exceptions import Unauthorized
+
+from models import connect_db, db, User, Feedback
+from forms import RegisterForm, LoginForm, FeedbackForm, DeleteForm
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres:///flask-feedback"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "shhhhh"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
+
 
 @app.route("/")
 def homepage():
     """Homepage of site; redirect to register."""
 
     return redirect("/register")
+
+
