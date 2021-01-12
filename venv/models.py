eda769258a997bb0d9e492a4f51304c2cd864415
Juan Cruz <jc23729@gmail.com>
2021-01-11 23:21:38 -0500

    Update models.py

---
+++ venv/models.py
@@ -0,0 +1,14 @@
+"""
+Part 1: Create User Model
+First, create a User model for SQLAlchemy. Put this in a models.py file.
+
+It should have the following columns:
+
+username - a unique primary key that is no longer than 20 characters.
+password - a not-nullable column that is text
+email - a not-nullable column that is unique and no longer than 50 characters.
+first_name - a not-nullable column that is no longer than 30 characters.
+last_name - a not-nullable column that is no longer than 30 characters.
+"""
+
+#### next steps would be create a base template
